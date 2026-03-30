25b81f2f118c6b9c259fb749ff5d76662f47d502
fabby <fabianmunga163@gmail.com>
2026-03-30 20:04:15 +0000

    Task 11.3: Refactored callback hell to Promises with chaining example

--- js/app.js
+++ js/app.js
@@ -66,3 +66,51 @@ getUserData(1, function(user) {
         });
     });
 });
+function getUserData(userId) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (userId > 0) {
+                resolve({ id: userId, name: "John" });
+            } else {
+                reject("Invalid user ID");
+            }
+        }, 1000);
+    });
+}
+
+function getUserPosts(userId) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve([
+                { id: 1, title: "Post 1" },
+                { id: 2, title: "Post 2" }
+            ]);
+        }, 1000);
+    });
+}
+
+function getPostComments(postId) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve([
+                { id: 1, text: "Great post!" },
+                { id: 2, text: "Thanks for sharing" }
+            ]);
+        }, 1000);
+    });
+}
+getUserData(1)
+    .then(user => {
+        console.log("User:", user);
+        return getUserPosts(user.id);
+    })
+    .then(posts => {
+        console.log("Posts:", posts);
+        return getPostComments(posts[0].id);
+    })
+    .then(comments => {
+        console.log("Comments:", comments);
+    })
+    .catch(error => {
+        console.error("Error:", error);
+    });
